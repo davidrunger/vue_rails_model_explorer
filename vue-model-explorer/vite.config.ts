12c74abd139f718bef27d95977a2c9e9836f8b13
David Runger <davidjrunger@gmail.com>
2025-03-20 02:36:19 -0500

    Use 'vite-plugin-dts' to emit a single 'index.d.ts' types file

--- vue-model-explorer/vite.config.ts
+++ vue-model-explorer/vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
-  plugins: [tailwindcss(), vue()],
+  plugins: [
+    tailwindcss(),
+    vue(),
+    dts({
+      rollupTypes: true,
+    }),
+  ],
   build: {
     lib: {
       entry: 'src/index.ts',
